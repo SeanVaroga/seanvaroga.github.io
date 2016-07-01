038868282aa64b113608caf54c93ee679c4e62f3
SeanVaroga <s.r.varoga@gmail.com>
2016-06-30 22:34:27 -0700

    Added Development & Development2
    Added Development & Development2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var $windowHeight = parseInt($(window).height());
 var $midHeight = parseInt($(".middle").height());
 var $topBottomHeight = ($windowHeight - $midHeight)/2;
+var $elementWidth = $(".name").outerWidth() + $(".site-anchors").outerWidth();
+var $centerWidth = $(window).width() - $elementWidth;
 $(".top, .bottom").css("height",$topBottomHeight);
 
 $(window).resize(function() {
@@ -9,3 +11,12 @@ $(window).resize(function() {
 	var $topBottomHeight = ($windowHeight - $midHeight)/2;
 	$(".top, .bottom").css("height",$topBottomHeight);
 });
+
+$(".body-content").css("margin-top",$("#nav").outerHeight());
+console.log($(".name").outerWidth());
+console.log($(".filler-text").outerWidth());
+console.log($(".site-anchors").outerWidth());
+console.log($(window).width());
+// $(".filler-text").css("width","1052px").css("padding","0");
+console.log($("#nav").outerWidth());
+console.log($centerWidth);
