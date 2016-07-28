48490ab6449a98fd2fb88f8806801daa53f0e6c7
Sean Varoga <s.r.varoga@gmail.com>
2016-07-28 11:20:06 -0700

    Testing commit with comment in line

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var $midHeight = parseInt($(".middle").height());
 var $topBottomHeight = ($windowHeight - $midHeight)/2;
 var $elementWidth = $(".name").outerWidth() + $(".site-anchors").outerWidth();
 var $centerWidth = $(window).width() - $elementWidth;
+var $projectPicHeight = $(".projects-img-p1");
 $(".top, .bottom").css("height",$topBottomHeight);
 
 $(window).resize(function() {
@@ -13,10 +14,5 @@ $(window).resize(function() {
 });
 
 $(".body-content").css("margin-top",$("#nav").outerHeight());
-console.log($(".name").outerWidth());
-console.log($(".filler-text").outerWidth());
-console.log($(".site-anchors").outerWidth());
-console.log($(window).width());
-// $(".filler-text").css("width","1052px").css("padding","0");
-console.log($("#nav").outerWidth());
-console.log($centerWidth);
+
+
